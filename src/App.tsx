3555dcf53f66b542020830ab051e1b7903e76641
lily <eshigh23@gmail.com>
2026-08-24 13:57:35 -0400

    API call set up with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [searchText, setSearchText] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+      console.log('searchText:', searchText)
+    }, [searchText])
+
+  const searchRepos = async (e: any) => {
+    e.preventDefault()
+
+    try {
+      setIsLoading(true)
+      const response = await axios.get("https://api.github.com/search/repositories", 
+        {
+          params: {
+            q: searchText
+          }
+        }
+      )
+      console.log('response:', response)
+
+    } catch (e) {
+      console.log('error:', e)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
-    <div>Github Repo Search</div>
+    <div>
+      <form onSubmit={(e)=> searchRepos(e)}>
+        <label> Search
+          <input
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </label>
+
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
